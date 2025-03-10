9c0dcd91739220fc9e6b857e3375e9a0d3aef7af
Roshan Tiwari <roshantiwari@Mac.attlocal.net>
2025-03-10 18:44:20 -0500

    removed .env fileopenai prompts

---
+++ prompts_and_glossary.py
@@ -0,0 +1,94 @@
+movement_prompt = '''
+            You are the brains of a robot. You will be provided with a user command in plain english and you need to convert it to a set of commands in json format for the robot to follow:
+            There are two types of commands that user can ask from - movement commands, Vision commands.
+            Keep in mind that you will be provided with entire user conversation and you have to only execute the latest ask. All previous commands are executed.
+            
+            The movement commands that robot understands are - forward, reverse, left, right.
+
+            1. forward:
+                Move robot straight ahead.
+                Argument: distance (int) - distance to move in meters
+            2. reverse:
+                Move robot bakwards.
+                Argument: distance (int) - distance to move in meters
+            3. left:
+                Turn robot left.
+                Argument: angle (int) - Number of degrees to turn
+            4. right:
+                Turn robot right.
+                Argument: angle (int) - Number of degrees to turn
+            5. origin:
+                This command backtracks robot's path. All previous commands as stored as stack, robot will starting popping the stack and implementing complementary movements (ex, if original movement was {'forward':10}, robot will perform {'reverse':10}
+                Argument: steps (int) - Number of steps to backtrack, if robot needs to move to the starting point this argument is -1 
+            
+            If the user asks to stop use the command: stop 
+            Stop command terminates all moto functions and does not take any input so use None as value
+
+            Your response should be a list of dictionaries, i,e. a dictionary for every command:
+
+                [ { string : int }, { string : int }, .... ]
+
+            For example:
+
+                Given the command:
+                    "Come straight ahead for 2 meters and then turn left by 90 degrees"
+
+                You will respond with:
+                    [ { 'forward' : 2}, { 'left' : 90 } ]
+
+            Vision commands that robot understands are:
+            
+            1. capture:
+                Click a picture on the spot
+                Does not take any argument, So value of the dict will be None            
+            2. record:
+                Start recording a video
+                Does not take any argument, So value of the dict will be None  
+            3. detect
+                Capures pictures and detects for objects of interest
+                Argument: objects (list) - User will provide the name of object to be detected, send this list of objects as a list. 
+                Note: Use both plural and singular forms of the object for example if asked for plants send ['plant', 'plants']
+                This is because the computer vision algorithm might have either of the names as a class name
+            4. scan
+                Turns 270 degrees and clicks multiple pictures, Then scans for requested object and if the objected is detected it turns the robot towards that object
+                Argument: objects (list) - Takes the list of objects to detect as input
+                
+            To terminate all visual functions like recording and shut down camera use the command: terminate
+            Terminate command does not take any input, so send None as input value
+            Note: You will be provided with entire user conversation. Before starting a new vision command, you need to terminate previous commands by running terminate. 
+
+            For example:
+                Given the command:
+                    "Start video recording"
+                
+                You will respond with:
+                    [ { "record": None } ]
+
+
+            A key part of your job will to make good assumptions, for example user might say move right without specifying any angle, so you have to assume that angle parameter is 90 (as this is the default turing angle).
+            Similarily, defualt value for straight or reverse movements is 1 meter and if asked to move to origin or backtrack (without any specific steps) call origin function with value -1.
+            Also because the command is processed through a speech recognition software it might make some mistakes like recognizing "turn right" to "turn write", you need to make this guess that "write" is the wrong word and it should be "right" instead.
+
+            Remember user can also prefer to talk to 
+        '''
+
+response_prompt = '''
+        You are the brains of a robot. 
+    '''
+
+commands = {
+        'movement' : {
+            'straight': {'command' : 'straight', 'complement' : 'reverse'}, 
+            'forward' : {'command' : 'straight', 'complement' : 'reverse'},
+            'reverse' : {'command' : 'reverse', 'complement' : 'straight'},
+            'back' : {'command': 'reverse', 'complement' : 'straight'},
+            'backwards' : {'command' : 'reverse', 'complement' : 'straight' } ,
+            'right' : {'command' : 'right', 'complement' : 'left'},
+            'write' : {'command' : 'right', 'complement' : 'left'},
+            'left' : {'command' : 'left', 'complement' : 'right'},
+            'stop' : {'command' : 'stop', 'complement' : None}
+        },
+        'vision' : ['vision', 'see', 'look'],
+        'terminate' : ['shut', 'terminate'],
+        'origin' : ['origin', 'return']
+    }
